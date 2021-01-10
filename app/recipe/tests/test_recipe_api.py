963ee49ce7c3cd462f9f9823648f07a777d6178b
Emilio <fernadezemiliov@gmail.com>
2021-01-09 19:30:27 -0600

    Added create recipe feature

--- app/recipe/tests/test_recipe_api.py
+++ app/recipe/tests/test_recipe_api.py
@@ -121,13 +121,13 @@ class PrivateRecipeApiTests(TestCase):
         tag2 = sample_tag(user=self.user, name='Dessert')
         payload = {
             'title': 'Avocado',
-            'tags': [tag1, tag2],
+            'tags': [tag1.id, tag2.id],
             'time_minutes': 60,
             'price': 20.00
         }
         res = self.client.post(RECIPE_URL, payload)
         self.assertEqual(res.status_code, status.HTTP_201_CREATED)
-        recipe = recipe.objects.get(id=res.data['id'])
+        recipe = Recipe.objects.get(id=res.data['id'])
         tags = recipe.tags.all()
         self.assertEqual(tags.count(), 2)
         self.assertIn(tag1, tags)
@@ -138,12 +138,12 @@ class PrivateRecipeApiTests(TestCase):
         ing2 = sample_ingredient(user=self.user, name='Ginger')
         payload = {
             'title': 'Thai prawn curry',
-            'ingredients': [ing1, ing2],
+            'ingredients': [ing1.id, ing2.id],
             'time_minutes': 20,
             'price': 7.00
         }
-        res = self.client.post(RECIPE_URL, status.HTTP_201_CREATED)
+        res = self.client.post(RECIPE_URL, payload)
         recipe = Recipe.objects.get(id=res.data['id'])
         ingredients = recipe.ingredients.all()
-        self.assertEqual(ing1, ingredients)
+        self.assertIn(ing1, ingredients)
         self.assertIn(ing2, ingredients)
